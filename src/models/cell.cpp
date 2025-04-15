79c3b446b35965406c79b992fb55467c0a04de93
Antoniel <segfault497@gmail.com>
2025-04-15 14:53:29 -0400

    feat: create cells

---
+++ src/models/cell.cpp
@@ -0,0 +1,20 @@
+#include "cell.hpp"
+#include <SDL3/SDL_rect.h>
+#include <iostream>
+
+Cell::Cell() { status = ALIVE; }
+
+Cell::Cell(float x, float y) {
+  status = ALIVE;
+  cell_graphic = new SDL_FRect{x, y, 10, 10};
+}
+void Cell::update_status(cell_status _status) { status = _status; }
+
+cell_status Cell::get_status() { return status; }
+
+SDL_FRect *Cell::get_cell_graphic() { return cell_graphic; }
+
+void Cell::update_position(float x, float y) {
+  cell_graphic->x = x;
+  cell_graphic->y = y;
+}
---
+++ src/models/cell.hpp
@@ -0,0 +1,17 @@
+#include <SDL3/SDL_rect.h>
+#pragma once
+enum cell_status { ALIVE, DEAD };
+
+class Cell {
+private:
+  cell_status status;
+  SDL_FRect *cell_graphic;
+
+public:
+  Cell();
+  Cell(float x, float y);
+  void update_status(cell_status _status);
+  cell_status get_status();
+  SDL_FRect *get_cell_graphic();
+  void update_position(float x, float y);
+};
